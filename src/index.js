48d5fc954a0b67715b0d99e0a6ed9ae635c1a3ec
Brandon Vergara <brandon@makingdevs.com>
2023-03-15 09:19:50 -0600

    [main] add button style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './styles.module.css'
 import Button from './components/Button'
+import ButtonStyle from './components/ButtonStyle'
 import './components/Button/button.css'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
 
-export { Button }
+export { Button, ButtonStyle }
